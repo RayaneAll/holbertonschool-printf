c11732f6a53bce0f35193e8123776a1726820959
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-28 21:10:02 +0100

    Change code

--- printf.c
+++ printf.c
@@ -2,9 +2,10 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int print_char(char c);
-int print_string(char *s);
-int print_number(int n);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_number(va_list args);
+int print_percent(va_list args);
 
 /**
  * _printf - Produces output according to a format
@@ -15,32 +16,54 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	char c;
-	char *s;
-	int n;
+	int (*print_func)(va_list);
+	char *ptr = (char *)format;
 
 	va_start(args, format);
-	while (format && *format)
+	while (ptr && *ptr)
 	{
-		if (*format != '%')
-			count += write(1, format, 1);
+		if (*ptr != '%')
+		{
+			count += write(1, ptr, 1);
+		}
 		else
 		{
-			format++;
-			if (*format == 'c')
-				count += print_char(va_arg(args, int));
-			else if (*format == 's')
-				count += print_string(va_arg(args, char *));
-			else if (*format == '%')
-				count += write(1, "%", 1);
-			else if (*format == 'd' || *format == 'i')
-			{
-				n = va_arg(args, int);
-				count += print_number(n);
-			}
+			ptr++;
+			print_func = get_print_func(*ptr);
+			if (print_func)
+				count += print_func(args);
+			else
+				count += write(1, ptr - 1, 2);
 		}
-		format++;
+		ptr++;
 	}
 	va_end(args);
 	return (count);
 }
+
+/**
+ * get_print_func - Selects the correct function for printing
+ * @s: The format specifier
+ * Return: A pointer to the printing function
+ */
+int (*get_print_func(char s))(va_list)
+{
+	print_t funcs[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_number},
+		{'i', print_number},
+		{'%', print_percent},
+		{0, NULL}
+	};
+	int i = 0;
+
+	while (funcs[i].spec)
+	{
+		if (funcs[i].spec == s)
+			return (funcs[i].f);
+		i++;
+	}
+	return (NULL);
+}
+
