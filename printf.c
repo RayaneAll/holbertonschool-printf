44ee5f5cbf691034656b3bd60162ed79cac35048
RayaneAll <9975@holbertonstudents.com>
2024-11-29 23:04:57 +0100

    New code for new condition of function printf

--- printf.c
+++ printf.c
@@ -2,12 +2,13 @@
 #include <stdarg.h>
 
 /**
- * _printf - Prints formatted output
- * @format: The format string containing characters and specifiers
+ * _printf - Produces output according to a format
+ * @format: Character string containing zero or more directives
  *
  * Description: Mimics the standard `printf` function. Processes the format
  * string to handle specifiers for characters (%c), strings (%s), integers
- * (%d, %i), and percent signs (%%).
+ * (%d, %i), and percent signs (%%). If `%` is followed by `\0`, it does not
+ * print anything.
  *
  * Return: Total number of characters printed
  */
@@ -24,15 +25,20 @@ int _printf(const char *format, ...)
 	va_list args;
 	int j, count = 0;
 
-	if (!format)
+	if (!format) /* Handle NULL format string */
 		return (-1);
 
 	va_start(args, format);
-	while (*format)
+
+	while (*format) /* Loop through each character in the format string */
 	{
-		if (*format == '%')
+		if (*format == '%') /* Handle format specifier */
 		{
 			format++;
+			if (*format == '\0') /* If '%' is followed by NULL, stop processing */
+				return (count); /* Do nothing, just return the count */
+
+			/* Check for valid specifier */
 			for (j = 0; print_types[j].type; j++)
 			{
 				if (*format == *(print_types[j].type))
@@ -41,16 +47,17 @@ int _printf(const char *format, ...)
 					break;
 				}
 			}
-			if (!print_types[j].type)
+
+			if (!print_types[j].type) /* If no valid specifier, print '%' and the character */
 				count += _putchar('%') + _putchar(*format);
 		}
-		else
+		else /* Print regular characters */
 		{
 			count += _putchar(*format);
 		}
-		format++;
+		format++; /* Move to the next character */
 	}
 
-	va_end(args);
+	va_end(args); /* Clean up variadic arguments */
 	return (count);
 }
