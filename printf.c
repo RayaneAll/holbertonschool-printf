43dfdbe5ce4f7c2c9618a3fdf8db09d34a414ec8
RayaneAll <9975@holbertonstudents.com>
2024-11-29 20:26:58 +0100

    Add condition function printf.c

--- printf.c
+++ printf.c
@@ -13,44 +13,49 @@
  */
 int _printf(const char *format, ...)
 {
-	PrintType_t print_types[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
-		{NULL, NULL}
-	};
-	va_list args;
-	int j, count = 0;
+    PrintType_t print_types[] = {
+        {"c", print_char},
+        {"s", print_string},
+        {"%", print_percent},
+        {"d", print_integer},
+        {"i", print_integer},
+        {NULL, NULL}
+    };
+    va_list args;
+    int j, count = 0;
 
-	if (!format)
-		return (-1);
+    if (!format)
+        return (-1);
 
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			for (j = 0; print_types[j].type; j++)
-			{
-				if (*format == *(print_types[j].type))
-				{
-					count += print_types[j].print_type_function(args);
-					break;
-				}
-			}
-			if (!print_types[j].type)
-				count += _putchar('%') + _putchar(*format);
-		}
-		else
-		{
-			count += _putchar(*format);
-		}
-		format++;
-	}
+    va_start(args, format);
 
-	va_end(args);
-	return (count);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            /* Check if '%' is the last character in the format string */
+            if (*format == '\0')
+                break;
+
+            for (j = 0; print_types[j].type; j++)
+            {
+                if (*format == *(print_types[j].type))
+                {
+                    count += print_types[j].print_type_function(args);
+                    break;
+                }
+            }
+            if (!print_types[j].type)
+                count += _putchar('%') + _putchar(*format);
+        }
+        else
+        {
+            count += _putchar(*format);
+        }
+        format++;
+    }
+
+    va_end(args);
+    return (count);
 }
