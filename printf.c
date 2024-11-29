284a18e247d47717fc6fbc086f908720f75758de
RayaneAll <9975@holbertonstudents.com>
2024-11-29 17:16:41 +0100

    Coorection code printf

--- printf.c
+++ printf.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/* Prototypes for printing functions */
 int print_char(va_list args);
 int print_string(va_list args);
 int print_number(va_list args);
@@ -10,60 +11,74 @@ int print_percent(va_list args);
 /**
  * _printf - Produces output according to a format
  * @format: Character string containing zero or more directives
- * Return: Number of characters printed (excluding null byte)
+ *
+ * Description: Mimics the functionality of the standard printf function.
+ * and the percent symbol (%%).
+ *
+ * Return: The total number of characters printed (excluding null byte).
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-	int (*print_func)(va_list);
-	char *ptr = (char *)format;
+	va_list args;                /* List to store variadic arguments */
+	int count = 0;               /* Counter for the total printed characters */
+	int (*print_func)(va_list);  /* Pointer to the corresponding print function */
+	char *ptr = (char *)format;  /* Pointer to traverse the format string */
 
+	/* Start variadic argument processing */
 	va_start(args, format);
+
+	/* Loop through the format string */
 	while (ptr && *ptr)
 	{
-		if (*ptr != '%')
+		if (*ptr != '%')  /* If not a specifier, write the character as is */
 		{
 			count += write(1, ptr, 1);
 		}
-		else
+		else  /* If it's a format specifier */
 		{
-			ptr++;
+			ptr++;  /* Move to the specifier character */
 			print_func = get_print_func(*ptr);
-			if (print_func)
+			if (print_func)  /* If a valid function is found, call it */
 				count += print_func(args);
-			else
+			else  /* If no valid function, print '%' and the specifier */
 				count += write(1, ptr - 1, 2);
 		}
-		ptr++;
+		ptr++;  /* Move to the next character in the format string */
 	}
+
+	/* Clean up variadic arguments */
 	va_end(args);
-	return (count);
+	return (count);  /* Return the total number of characters printed */
 }
 
 /**
  * get_print_func - Selects the correct function for printing
- * @s: The format specifier
- * Return: A pointer to the printing function
+ * @s: The format specifier character
+ *
+ * Description: Matches the format specifier with its corresponding function
+ * from an array of structures.
+ *
+ * Return: A pointer to the appropriate printing function, or NULL if no match.
  */
 int (*get_print_func(char s))(va_list)
 {
-	print_t funcs[] = {
+	print_t funcs[] = {  /* Array mapping specifiers to their functions */
 		{'c', print_char},
 		{'s', print_string},
 		{'d', print_number},
 		{'i', print_number},
 		{'%', print_percent},
-		{0, NULL}
+		{0, NULL}  /* Sentinel value to mark the end of the array */
 	};
 	int i = 0;
 
+	/* Loop through the array to find a matching specifier */
 	while (funcs[i].spec)
 	{
 		if (funcs[i].spec == s)
-			return (funcs[i].f);
+			return (funcs[i].f);  /* Return the matching function */
 		i++;
 	}
-	return (NULL);
-}
 
+	return (NULL);  /* Return NULL if no match is found */
+}
